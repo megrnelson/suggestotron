58123e942d649ccee2deeb9293ae3213c3139ef5
megrnelson <nelsonmegr@gmail.com>
2014-04-12 10:53:55 -0700

    Added upvotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Suggestotron::Application.routes.draw do
-  resources :topics
-
   root 'topics#index'
+
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
